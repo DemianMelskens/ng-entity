a0a80eb8ecf117f3288baf1cc47d38df9d66df14
Demian <dmelskens@quintor.nl>
2021-07-10 15:52:10 +0200

    added shape component still working on implementation of that and the renderer2d

---
+++ src/ng-entity/core/components/shape.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "../scene";
+import {Shape} from "../../shared/domain/shape";
+
+export class ShapeComponent implements Component {
+  constructor(public shape: Shape) {
+  }
+}
